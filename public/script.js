c1ee270406aaff57a220577b697ddc5c1d3b7e7e
Gor0o0 <moasobakaposholvon@gmail.com>
2026-04-20 09:54:48 +0300

    update

--- public/script.js
+++ public/script.js
@@ -1,10 +1,26 @@
-const clickBtn = document.getElementById('clickBtn');
-clickBtn.addEventListener('click', () => {});
+const logInput = document.getElementById('input-login').value;
+const passInput = document.getElementById('input-pass').value;
+
+const logInBtn = document.getElementById('logInBtn');
+logInBtn.addEventListener('click', () => {});
 
 async function ResponseServer() { //> это функция для отправки запроса на сервер
-    const response = await fetch('/api', {
+    const res = await fetch('api/', {
         method: 'POST',
         headers:{"Content-Type": "application/json"},
         body: JSON.stringify({message: 'Hello World'})
-    })
+    });
+}
+
+
+async function singIn(){
+    
+}
+
+async function singUp(){
+    const res = await fetch('api/register', {
+        method: 'POST',
+        headers:{"Content-Type": "application/json"},
+        body: JSON.stringify({login:login, password: password})
+    });
 }
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 // -=-=-=|Модульный js|=-=-=-
-import express from 'express' 
-import path from 'path'
-import { fileUrlToPath } from 'url'
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-const __filename = fileUrlToPath(import.meta.url)
+const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 
@@ -11,10 +11,12 @@ const __dirname = path.dirname(__filename)
 const app = express()
 const port = 3000;
 
+// -=-= Middleware | промежуточные слои. Подобие импортов =-=-
 app.use(express.json()); //> это для парсинга JSON-тела запроса
 app.use(express.urlencoded({ extended: true })); //> это для парсинга URL-кодированных тела запроса | extended true - для парсинга вложенных объектов
 app.use(express.static('public')); //> это для предоставления статических файлов из директории public
 
+// -=-= Routers | Обработчики запросов =-=-
 app.get('/', (req, res) => {
   res.sendFile(__dirname, '/public/index.html') //> это для отправки файла index.html или другого указаного
 });
@@ -33,6 +35,7 @@ app.delete('/api', (req, res) => {
   res.send('Delete World')
 });
 
+// -=-= Server | Запуск сервера =-=-
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`)
 })
